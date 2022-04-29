0135b932910194ffd2c9b1ce142162fb59ed5e86
Ricardo Milan <ricardo@intercambiodireto.com>
2022-04-29 08:54:55 -0300

    Add project files.

---
+++ MainApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MainApp;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        HelloFrom("Hello World!");
+    }
+
+    static partial void HelloFrom(string name);
+}
